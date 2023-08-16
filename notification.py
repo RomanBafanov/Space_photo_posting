6dddc3e83054fb97eaedb449592e79622353a6a8
Roman Bafanov <108800547+RomanBafanov@users.noreply.github.com>
2023-08-16 16:09:52 +0300

    changed notification.py
    chat_id removed from code

--- notification.py
+++ notification.py
@@ -7,13 +7,13 @@ import time
 import os
 
 
-def get_public_of_photos(args, bot):
+def get_public_of_photos(args, bot, chat_id):
     abs_path = os.path.abspath('images')
     images = os.listdir(abs_path)
     random.shuffle(images)
     for image in images:
         abs_path = os.path.abspath(f'images/{image}')
-        bot.send_photo(chat_id='-1001692357239', photo=open(abs_path, 'rb'))
+        bot.send_photo(chat_id=chat_id, photo=open(abs_path, 'rb'))
         time.sleep(args)
 
     bot.infinity_polling(skip_pending=True)
@@ -34,10 +34,11 @@ def main():
     state_storage = StateMemoryStorage()
     load_dotenv()
     token = os.getenv('BOT_TOKEN')
+    chat_id = os.getenv('BOT_CHAT_ID')
 
     bot = telebot.TeleBot(token, state_storage=state_storage)
 
-    get_public_of_photos(args, bot)
+    get_public_of_photos(args, bot, chat_id)
 
 
 if __name__ == '__main__':
