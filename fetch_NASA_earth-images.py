41df2991e4b43ba5c1531382833c1ad882f79aae
Roman Bafanov <108800547+RomanBafanov@users.noreply.github.com>
2023-08-17 18:05:18 +0300

    changed fetch_NASA_earth-images.py
    a number is given a variable name

--- fetch_NASA_earth-images.py
+++ fetch_NASA_earth-images.py
@@ -5,7 +5,7 @@ import argparse
 
 
 URL = 'https://api.nasa.gov/EPIC/archive/natural'
-
+NUMBER_OF_PHOTOS = 5
 
 def get_earth_images(date, image, idx, api_key):
     url = f'{URL}/{date[0]}/{date[1]}/{date[2]}/png/{image}.png'
@@ -45,7 +45,7 @@ def main():
 
     load_dotenv()
     api_key = os.getenv('API_KEY_NASA')
-    for count in range(5):
+    for count in range(NUMBER_OF_PHOTOS):
         date, image, api_key = get_image_date(api_key, count)
 
         url, filename = get_earth_images(date, image, count, api_key)
